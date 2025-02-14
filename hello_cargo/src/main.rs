6f505edf9617f01ec5046615fa42be0a1587072a
Nuntapong Siripanyawong <nuntapong@odds.team>
2025-02-14 23:40:00 +0700

    Ignore target folder

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
+    let number: i32 = 5;
+
+    println!("The value of number is: {}", number);
     println!("Hello, world!");
 }
--- hello_cargo/target/debug/deps/hello_cargo-b818b32762dba0be.d
+++
@@ -1,5 +0,0 @@
-/Users/nuntapong/workspace/learning/rust/hello_cargo/target/debug/deps/hello_cargo-b818b32762dba0be: src/main.rs
-
-/Users/nuntapong/workspace/learning/rust/hello_cargo/target/debug/deps/hello_cargo-b818b32762dba0be.d: src/main.rs
-
-src/main.rs:
