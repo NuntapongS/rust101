3244fd1789a4db7f27df2ce943da07b52e6eda48
Nuntapong Siripanyawong <nuntapong@odds.team>
2025-02-14 23:44:19 +0700

    Enhance main function with additional output and increment logic

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,6 +1,14 @@
 fn main() {
     let number: i32 = 5;
+    println!("Hello, world!");
 
     println!("The value of number is: {}", number);
-    println!("Hello, world!");
+
+    println!("--------------------------------------");
+
+    println!("The value of number will plus 1");
+
+    let number = number + 1;
+
+    println!("The value of number is: {}", number);
 }
