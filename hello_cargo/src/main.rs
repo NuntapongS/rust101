9e474b44a91b5bb8c6b849e9b97bb4fbe00d9143
Nuntapong Siripanyawong <nuntapong@odds.team>
2025-02-15 12:15:18 +0700

    display value from variable

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,11 +1,16 @@
 use std::io;
 
 fn main() {
+    let x = 5;
+    let y = 2;
+
+    println!("The value of x is: {x} and y is: {y}");
+
+    let mut guess = String::new();
+
     println!("Guess the number!");
 
     println!("Please input your guess : ");
-
-    let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
