0f8f9e55e68604fd366dbc7b920e647c8a9f5c1d
Nuntapong Siripanyawong <nuntapong@odds.team>
2025-02-15 09:06:05 +0700

    update main function to implement a number guessing game

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,14 +1,15 @@
+use std::io;
+
 fn main() {
-    let number: i32 = 5;
-    println!("Hello, world!");
+    println!("Guess the number!");
 
-    println!("The value of number is: {}", number);
+    println!("Please input your guess : ");
 
-    println!("--------------------------------------");
+    let mut guess = String::new();
 
-    println!("The value of number will plus 1");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
-    let number = number + 1;
-
-    println!("The value of number is: {}", number);
+    println!("You guessed: {}", guess);
 }
